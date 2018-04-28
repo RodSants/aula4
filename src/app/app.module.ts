910c382ca9f206fe99a4716785bdb335b54644be
RodSants <rod16.03.92@gmail.com>
2018-04-27 21:16:35 -0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ServicoComponent } from './servico/servico.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServicoComponent,
+    ContatoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
